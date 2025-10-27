3ac34b98b2e3c88e26a2d4f468d4a8063ce6347c
Theo Zadra <boostlap123@gmail.com>
2025-10-27 13:40:52 +0000

    feat: implementati componenti Header, Footer e NavBar, e creato modulo Bindings con template e stili

--- corso/src/app/app.ts
+++ corso/src/app/app.ts
@@ -1,9 +1,14 @@
+import { BindingParser } from '@angular/compiler';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { NavBar } from './nav-bar/nav-bar';
+import { Bindings } from './bindings/bindings';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, NavBar, Bindings ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ corso/src/app/bindings/bindings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-bindings',
+	imports: [],
+	templateUrl: './bindings.html',
+	styleUrls: ['./bindings.css']
+})
+export class Bindings {
+
+}
