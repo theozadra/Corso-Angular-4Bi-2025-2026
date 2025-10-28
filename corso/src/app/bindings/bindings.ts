16c06e0bb4740b17154cee1393f1c78d23685e7d
Theo Zadra <boostlap123@gmail.com>
2025-10-28 07:44:21 +0000

    feat: aggiunti metodi per incremento e decremento del contatore nel componente bindings

--- corso/src/app/bindings/bindings.ts
+++ corso/src/app/bindings/bindings.ts
@@ -22,8 +22,16 @@ export class Bindings {
 		//Il metodo deve aumentare di 1 il valore di contatore.
 		// 1. Primo sistema: non consigliato
 		this.contatore.set(this.contatore() + 1);
-		// 2. sistema più usato:
+		// 2. sistema più usato: Si tratta di un espressione lamba, a sinistra dell'uguale dichiaro una variabile
+		// che verrà inizializzata con il valore corrente gestito da signal
+		// a destra del > scrivo l'istruzione che mi consente di calcolare il nuovo valore
 		this.contatore.update(valoreAttuale => valoreAttuale + 1);
+		// La funziona LAMBDA può prevedere anche un blocco d'istruzioni.
+		// Nel blocco deve essere presente l'istruzione return, che restituirà il nuovo valore per il signal
+
+	}
+	decrementa(): void{
+		this.contatore.update(valoreAttuale => valoreAttuale - 1);
 
 	}
 }
