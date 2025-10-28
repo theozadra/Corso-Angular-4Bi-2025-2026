40c5f5c43d8a002d62255ea277c3c9122fe69475
Theo Zadra <boostlap123@gmail.com>
2025-10-28 07:32:23 +0000

    Refactor code structure for improved readability and maintainability

--- corso/src/app/bindings/bindings.ts
+++ corso/src/app/bindings/bindings.ts
@@ -7,11 +7,23 @@ import { Component, signal, WritableSignal } from '@angular/core';
 	styleUrls: ['./bindings.css']
 })
 export class Bindings {
+
 	//Dichiaro il signal writable
 	studente: WritableSignal<string> = signal('Theo Zadra');
+	contatore: WritableSignal<number> = signal(0);
+
 
 	// Aggiungo il metodo cambia nome
 	cambiaNome(): void {
 		this.studente.set('Luca Boschi');
+	}
+
+	incrementa(): void{
+		//Il metodo deve aumentare di 1 il valore di contatore.
+		// 1. Primo sistema: non consigliato
+		this.contatore.set(this.contatore() + 1);
+		// 2. sistema più usato:
+		this.contatore.update(valoreAttuale => valoreAttuale + 1);
+
 	}
 }
